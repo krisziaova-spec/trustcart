659038cfc175a626431e31e099ddce839636d62c
krisziaova-spec <kriszia.ova@gmail.com>
2026-06-07 00:42:06 +0800

    Update BuyerAccountRepository.java

--- src/main/java/com/trustcart/repository/BuyerAccountRepository.java
+++ src/main/java/com/trustcart/repository/BuyerAccountRepository.java
@@ -2,9 +2,15 @@ package com.trustcart.repository;
 
 import com.trustcart.model.BuyerAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.Optional;
 
 public interface BuyerAccountRepository extends JpaRepository<BuyerAccount, Long> {
-    Optional<BuyerAccount> findByEmailIgnoreCase(String email);
+    List<BuyerAccount> findAllByEmailIgnoreCaseOrderByIdAsc(String email);
+
+    default Optional<BuyerAccount> findByEmailIgnoreCase(String email) {
+        return findAllByEmailIgnoreCaseOrderByIdAsc(email).stream().findFirst();
+    }
+
     boolean existsByEmailIgnoreCase(String email);
 }
